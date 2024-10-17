4ab3e4b035d935bc8f5259d8a78f7e97520535f0
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-18 01:48:22 +0300

    no of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,22 +4,33 @@ def minOperations(n):
     '''Function computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    if not isinstance(n, int):
-        return 0
-    ops_count = 0
+    pasted_chars = 1
     clipboard = 0
-    done = 1
-    while done < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif clipboard > 0:
-            done += clipboard
-            ops_count += 1
+    counter = 0
 
-        return ops_count
+    while pasted_chars < n:
+        if clipboard == 0:
+            clipboard = pasted_chars
+            counter += 1
+
+        if pasted_chars == 1:
+            pasted_chars += clipboard
+            counter += 1
+            continue
+
+        remaining = n - pasted_chars
+        if remaining < clipboard:
+            return 0
+
+        if remaining % pasted_chars != 0:
+            pasted_chars += clipboard
+            counter += 1
+        else:
+            clipboard = pasted_chars
+            pasted_chars += clipboard
+            counter += 2
+
+    if pasted_chars == n:
+        return counter
+    else:
+        return 0
